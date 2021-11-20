64eb62b8b604001c6ff6311f9899477dece90f05
dylangrech@innovativecodes.com <dylangrech@innovativecodes.com>
2021-11-20 21:36:59 +0100

    Service Tests

--- src/services/Orders.php
+++ src/services/Orders.php
@@ -3,6 +3,7 @@
 namespace Lifeboat\Services;
 
 use Lifeboat\Exceptions\ApiException;
+use Lifeboat\Exceptions\InvalidArgumentException;
 use Lifeboat\Exceptions\OAuthException;
 use Lifeboat\Models\Order;
 use Lifeboat\Resource\ListResource;
@@ -19,13 +20,23 @@ class Orders extends ApiService {
     const PERIOD_30     = '30';
     const PERIOD_90     = '90';
     const PERIOD_120    = '120';
+    const VALID_PERIODS = [
+        self::PERIOD_ALL, self::PERIOD_DAY, self::PERIOD_7,
+        self::PERIOD_30, self::PERIOD_90, self::PERIOD_120
+    ];
 
     const STATUS_OPEN   = 1;
     const STATUS_PAID   = 2;
+    const VALID_STATUSES = [
+        self::STATUS_OPEN, self::STATUS_PAID
+    ];
 
     const FULFILLMENT_PENDING       = 1;
     const FULFILLMENT_FULFILLED     = 2;
     const FULFILLMENT_DELIVERED     = 3;
+    const VALID_FULFILLMENT_STATUSES = [
+        self::FULFILLMENT_PENDING, self::FULFILLMENT_FULFILLED, self::FULFILLMENT_DELIVERED
+    ];
 
     const FULFILLMENT_SHIP          = 0;
     const FULFILLMENT_DELIVER       = 1;
@@ -82,6 +93,18 @@ class Orders extends ApiService {
         int $status = self::STATUS_PAID,
         int $fulfillment = self::FULFILLMENT_PENDING
     ): ListResource {
+        if (!in_array($period, self::VALID_PERIODS)) {
+            throw new InvalidArgumentException("Orders::all expects parameter 1 to be a valid period");
+        }
+
+        if (!in_array($status, self::VALID_STATUSES)) {
+            throw new InvalidArgumentException("Orders::all expects parameter 2 to be a valid status");
+        }
+
+        if (!in_array($fulfillment, self::VALID_FULFILLMENT_STATUSES)) {
+            throw new InvalidArgumentException("Orders::all expects parameter 3 to be a valid fulfillment status");
+        }
+
         $data = [
             'period'        => $period,
             'status'        => $status,
---
+++ tests/services/ServicesTest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Lifeboat\Tests;
+
+use Lifeboat\Exceptions\InvalidArgumentException;
+use Lifeboat\Resource\ListResource;
+use Lifeboat\Resource\SimpleList;
+use Lifeboat\Services\Addresses;
+use Lifeboat\Services\Collections;
+use Lifeboat\Services\Customers;
+use Lifeboat\Services\CustomPages;
+use Lifeboat\Services\DeliveryZones;
+use Lifeboat\Services\Orders;
+use Lifeboat\Services\Pages;
+
+class ServicesTest extends TestCase {
+
+    /**
+     * @test
+     * @covers \Lifeboat\Services\Addresses::all
+     */
+    public function test_address_all()
+    {
+        $service = new Addresses($this->getMockClient());
+        $this->check_curl_all(
+            $service->all('xxx'),
+            'api/addresses/all',
+            ListResource::class,
+            ['search' => 'xxx']
+        );
+    }
+
+    /**
+     * @test
+     * @covers \Lifeboat\Services\Collections::all
+     */
+    public function test_collections_all()
+    {
+        $service = new Collections($this->getMockClient());
+        $this->check_curl_all($service->all(), 'api/collections/all');
+    }
+
+    /**
+     * @test
+     * @covers \Lifeboat\Services\Customers::all
+     */
+    public function test_customers_all()
+    {
+        $service = new Customers($this->getMockClient());
+        $this->check_curl_all(
+            $service->all('xxx'),
+            'api/customers/all',
+            ListResource::class,
+            ['search' => 'xxx']
+        );
+    }
+
+    /**
+     * @test
+     * @covers \Lifeboat\Services\CustomPages::all
+     */
+    public function test_custom_pages_all()
+    {
+        $service = new CustomPages($this->getMockClient());
+        $this->check_curl_all(
+            $service->all('xxx'),
+            'api/pages/page/all',
+            ListResource::class,
+            ['search' => 'xxx', 'sort' => CustomPages::SORT_DEFAULT]
+        );
+
+        try {
+            $service->all('xxx', 'xxx');
+            $this->fail('CustomPages::all() parameter 2 should be a valid sort');
+        } catch (InvalidArgumentException $e) {
+            // Error should have been thrown
+        }
+    }
+
+    /**
+     * @test
+     * @covers \Lifeboat\Services\DeliveryZones::all
+     */
+    public function test_delivery_zones_all()
+    {
+        $service = new DeliveryZones($this->getMockClient());
+        $this->check_curl_all($service->all(), 'api/delivery-zones/all');
+    }
+
+    /**
+     * @test
+     * @covers \Lifeboat\Services\Orders::all
+     */
+    public function test_orders_all()
+    {
+        $params = ['period' => Orders::PERIOD_7, 'status' => Orders::STATUS_PAID, 'fulfillment' => Orders::FULFILLMENT_PENDING];
+
+        $service = new Orders($this->getMockClient());
+        $this->check_curl_all($service->all(), 'api/orders/all', ListResource::class, $params);
+
+        try {
+            $service->all('xxx');
+            $this->fail('Orders::all expects parameter 1 to be a valid period');
+        } catch (InvalidArgumentException $e) {
+            // Error should have been thrown
+        }
+
+        try {
+            $service->all(Orders::PERIOD_7, -1);
+            $this->fail('Orders::all expects parameter 2 to be a valid status');
+        } catch (InvalidArgumentException $e) {
+            // Error should have been thrown
+        }
+
+        try {
+            $service->all(Orders::PERIOD_7, Orders::STATUS_PAID, -1);
+            $this->fail('Orders::all expects parameter 3 to be a valid fulfillment status');
+        } catch (InvalidArgumentException $e) {
+            // Error should have been thrown
+        }
+    }
+
+    /**
+     * @test
+     * @covers \Lifeboat\Services\Pages::all
+     */
+    public function test_pages_all()
+    {
+        $service = new Pages($this->getMockClient());
+        $this->check_curl_all($service->all(), 'api/pages/all', SimpleList::class);
+    }
+
+    /**
+     * @covers \Lifeboat\Resource\ListResource::getClient
+     * @covers \Lifeboat\Resource\ListResource::setClient
+     * @covers \Lifeboat\Resource\ListResource::getURL
+     * @covers \Lifeboat\Resource\ListResource::setURL
+     * @covers \Lifeboat\Resource\ListResource::getParams
+     * @covers \Lifeboat\Resource\ListResource::setParams
+     *
+     * @param ListResource $all
+     * @param string $list_class
+     * @param string $url
+     * @param array $params
+     */
+    private function check_curl_all(
+        ListResource $all,
+        string $url,
+        string $list_class = ListResource::class,
+        array $params = []
+    ){
+        $this->assertInstanceOf($list_class, $all);
+        $this->assertEquals($url, $all->getURL());
+        $this->assertEquals($this->getMockClient(), $all->getClient());
+        $this->assertEquals($params, $all->getParams());
+    }
+}
